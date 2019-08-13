91b58917599135fe43d9c22fe76de1ea96f834b3
Sean Boyle <sean94@gmail.com>
2019-08-13 23:27:49 +0100

    Added the complementay colors file to the colors folder

---
+++ Chapter_1_Color/01_complementary_colors/js/sketch.js
@@ -0,0 +1,33 @@
+// Creative coding - Complemtary colors
+function setup() {
+
+  let canvas = createCanvas(windowWidth,windowHeight);
+  colorMode(RGB, 300, 200, 0);
+  rectMode(CENTER);
+
+}
+
+function draw() {
+  background(200 - (mouseY / 4), 100, 220);
+  translate(width / 2, height / 2);
+  fill(mouseY / 4, 1, 1);
+  noStroke();
+  //use of a function to create the shape based on the mouseX mouseY
+  polygon (0, 0, mouseX, 5);
+}
+
+
+function keyPressed() {
+  if (key == 's' || key == 'S') saveCanvas(canvas, gd.timestamp() + '_MouseX_' + mouseX + '_MouseY_' + mouseY, 'png');
+ }
+
+function polygon(x, y, radius, noOfPoints) {
+  let angle = TWO_PI / noOfPoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
--- Chapter_1_Color/README.md
+++ Chapter_1_Color/README.md
@@ -3,3 +3,4 @@
 ## This chapter shows some the different color features of P5 such as learning how to use the pixel array and learing how to learp colors inorder to make new ones
 ---
 
+1. [Complementry Colors](https://sb1994.github.io/Creative_Code_Portfolio/Chapter_1_Color/01_complementary_colors/index.html)
